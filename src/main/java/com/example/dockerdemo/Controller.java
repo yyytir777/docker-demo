b2b0fba1af1e432a78c1f7e480e811452dccc209
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-02-14 22:45:16 +0900

    controller 추가

---
+++ src/main/java/com/example/dockerdemo/Controller.java
@@ -0,0 +1,13 @@
+package com.example.dockerdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping
+    public String test() {
+        return "Test";
+    }
+}
