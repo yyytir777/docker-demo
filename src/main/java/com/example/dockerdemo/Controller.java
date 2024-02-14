884470626042e4caff9f68ea508fe7ec88a98c22
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-02-15 01:03:53 +0900

    수정

--- src/main/java/com/example/dockerdemo/Controller.java
+++ src/main/java/com/example/dockerdemo/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @RequestMapping("/test")
+    @RequestMapping()
     public String test() {
         return "Test";
     }
